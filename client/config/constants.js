e861112637d6f1fe1abf518ffbb19de080d9e4a8
Clyde G Marl <marl.clyde@gmail.com>
2015-06-07 14:28:20 -0700

    switch nodeUrl back to local host after deploy

--- client/config/constants.js
+++ client/config/constants.js
@@ -2,4 +2,5 @@
 
 angular.module('convergence')
 .constant('firebaseUrl', 'https://clyde-marl.firebaseio.com/')
-.constant('nodeUrl', 'https://fast-ridge-6950.herokuapp.com/');
+// .constant('nodeUrl', 'https://fast-ridge-6950.herokuapp.com/');
+.constant('nodeUrl', 'http://ubuntu:8000');
